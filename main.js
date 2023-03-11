260a4a12e14a908d152955bc472cd3e86ec8b362
TayAki79 <a.tayarat@gmx.net>
2023-03-11 11:39:04 +0100

    hamburgerReady

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const homeSection = document.querySelector("#home");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    homeSection.classList.toggle("active");
 }
